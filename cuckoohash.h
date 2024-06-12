27ae8402f6406726496c93e854cbffdd322615f3
mishanyapank <146870999+mishanyapank@users.noreply.github.com>
2024-06-12 14:08:02 +0200

    Update cuckoohash.h

--- cuckoohash.h
+++ cuckoohash.h
@@ -9,23 +9,23 @@ using namespace std;
 
 class CuckooHash {
 private:
-    size_t size;
-    pair<int, int>* table1;
-    pair<int, int>* table2;
-    int hashFunction1(int key);
-    int hashFunction2(int key);
-    void resizeTable();
+    size_t size; // Rozmiar tablic mieszających
+    pair<int, int>* table1; // Pierwsza tablica mieszająca
+    pair<int, int>* table2; // Druga tablica mieszająca
+    int hashFunction1(int key); // Pierwsza funkcja mieszająca
+    int hashFunction2(int key); // Druga funkcja mieszająca
+    void resizeTable(); // Funkcja do zmiany rozmiaru tablicy
 
 public:
-    int currentSize, r, s;
-    CuckooHash(size_t size);
-    ~CuckooHash();
-    bool insert(int key, int value);
-    bool search(int key, int& value);
-    bool remove(int key);
-    void display();
-    void addToTable(int liczba);
-    void clear();
+    int currentSize, r, s; // Aktualny rozmiar tablicy, zmienne pomocnicze do usuwania i szukania
+    CuckooHash(size_t size); // Konstruktor
+    ~CuckooHash(); // Destruktor
+    bool insert(int key, int value); // Funkcja wstawiająca element do tablicy
+    bool search(int key, int& value); // Funkcja szukająca elementu w tablicy
+    bool remove(int key); // Funkcja usuwająca element z tablicy
+    void display(); // Funkcja wyświetlająca zawartość tablicy
+    void addToTable(int liczba); // Funkcja dodająca losowe elementy do tablicy
+    void clear(); // Funkcja czyszcząca tablicę
 };
 
 #endif
